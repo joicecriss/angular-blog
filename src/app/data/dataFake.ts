7f59811a3dd53840d5902e7ecd1e9f319d39e2a1
Joice Cristina <joice1509oliveira@gmail.com>
2023-10-03 10:36:02 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "title": "Novo Homem de Ferro anunciado!",
+    "description": "Marvel Studios anuncia novo filme do Homem de Ferro."
+  },
+  {
+    "id": "2",
+    "photo": "aaa",
+    "title": "saiu novo filme",
+    "description": "ola"
+  }
+]
